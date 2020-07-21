97b25a5978ec3233a87b2b980daff1f936437a92
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-07-21 18:25:55 -0400

    feat: control updates (back/forward, verification)

--- demo/core/scripts/modules/binds.js
+++ demo/core/scripts/modules/binds.js
@@ -2,16 +2,28 @@
 (function () {
     document.body.addEventListener('keydown', event => {
         let k = event.which;
-        if (k == 39 || k == 32) nextSlide() // right arrow key/spacebar
+        if (k == 39 || k == 32) nextSlide() // right arrow key/space bar
+        if (k == 37) previousSlide()
     });
 
     const nextSlide = () => {
-        if (slideContent.length !== 0) {
-            let currentSlide = document.querySelector('.main').firstChild;
-            let current = slides.indexOf(currentSlide.dataset.slideName);
+        let currentSlide = document.querySelector('.main').firstChild;
+        let current = slides.indexOf(currentSlide.dataset.slideName);
 
+        if (slideContent.length !== 0 && current != slides.length - 1) {
             currentSlide.remove();
             document.querySelector('.main').appendChild(slideContent[current + 1]);
+        }
+    }
+
+    const previousSlide = () => {
+        let currentSlide = document.querySelector('.main').firstChild;
+        let current = slides.indexOf(currentSlide.dataset.slideName);
+
+        if (slideContent.length != 0 && current != 0) {
+
+            currentSlide.remove();
+            document.querySelector('.main').appendChild(slideContent[current - 1]);
         }
     }
 }());
