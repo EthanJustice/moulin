1b827ad56908063776eaa7a1dd2a2e6fc08d7d40
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-07-22 12:25:44 -0400

    fix: switch caching to prod mode

--- demo/core/scripts/modules/template.js
+++ demo/core/scripts/modules/template.js
@@ -6,19 +6,23 @@ const load = async (name) => {
 		return renderJS(scopeCSS(content.querySelector('div')));
 	}
 
-	return await caches.open(`moulin-${config.version}`).then((cache) => {
-		return cache.keys(`${name}.html`).then((data) => {
-			if (data.length == 0) {
-				return cache.add(`${name}.html`).then(() => {
-					return fetch(`${name}.html`).then(resp => resp.text()).then(element => parse(element));
-				});
-			} else {
-				return data.map(item => {
-					return cache.match(item.url).then((resp) => {
-						return resp.clone().text().then((element) => { return parse(element) })
+	if (config.prod == true) {
+		return await caches.open(`moulin-${config.version}`).then((cache) => {
+			return cache.keys(`${name}.html`).then((data) => {
+				if (data.length == 0) {
+					return cache.add(`${name}.html`).then(() => {
+						return fetch(`${name}.html`).then(resp => resp.text()).then(element => parse(element));
 					});
-				});
-			}
+				} else {
+					return data.map(item => {
+						return cache.match(item.url).then((resp) => {
+							return resp.clone().text().then((element) => { return parse(element) })
+						});
+					});
+				}
+			});
 		});
-	});
+	} else {
+		return await fetch(`${name}.html`).then(resp => { return resp.text() }).then(data => { return parse(data) })
+	}
 }
