d82960ddc049de8d16f8b6a3ba4693d67904ab6f
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-07-24 18:18:55 -0400

    fix(modules/parse): scoped css parser not working
    Fixed the scoped CSS parser not working because it was looking through all the properties and selectors, rather than just the selectors

--- src/scripts/modules/parse.js
+++ src/scripts/modules/parse.js
@@ -5,13 +5,13 @@ const scopeCSS = (element) => {
     if (!element.querySelector('style')) { return element }
     let sheet = element.querySelector('style');
     Object.values(sheet.sheet.rules).forEach(item => {
-        if (!item.cssText.includes('@') && !item.cssText.includes(':')) {
-            element.querySelectorAll(item.selectorText).forEach(child => {
+        if (!item.selectorText.includes('@') && !item.selectorText.includes(':')) {
+            element.querySelectorAll(`${item.selectorText}`).forEach(child => {
                 Object.values(item.style).forEach(prop => {
                     child.style[prop] = item.style[prop];
                 });
             });
-        } else if (item.cssText.includes(':')) {
+        } else if (item.selectorText.includes(':')) {
             document.styleSheets[0].insertRule(item.cssText, 0);
         }
     });
